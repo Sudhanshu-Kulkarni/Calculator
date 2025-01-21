b1042aa4279b9c28f180368d21a87bf84c48b81a
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-01-21 18:35:27 +0530

    Implemented a method to take arrays as input for calculation in UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -13,4 +13,19 @@ class UserInput{
 
 		return numbers;
 	}
+
+	// Take an array as input
+    int[] userInputArray() {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter the size of the array:");
+        int size = scan.nextInt();
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scan.nextInt();
+        }
+
+        return array;
+    }
 }
