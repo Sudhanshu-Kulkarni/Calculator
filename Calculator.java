e26b85c0283283db526abcda964ee0aaf96720a1
Sudhanshu-Kulkarni <sudhanshu28.kulkarni@gmail.com>
2025-01-21 18:31:38 +0530

    Implemented Fibonacci sequence generation logic in Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -27,7 +27,16 @@ class Calculator{
         }
 	}
 
-	
+	// Fibonacci sequence
+    int[] fibonacci(int n) {
+        int[] fib = new int[n];
+        fib[0] = 0;
+        if (n > 1) fib[1] = 1;
+        for (int i = 2; i < n; i++) {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+        return fib;
+    }
 
 }
 
